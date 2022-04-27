d46b0af232fabae68bd198d39c6a47d5c53bbeea
heoeunse <gjdmstp0311@naver.com>
2022-04-27 20:06:42 +0900

    js

---
+++ 추가스터디js-2/js10-object3.js
@@ -0,0 +1,10 @@
+let student1 = {
+    koreanScore:90,
+    "englishScore":70,
+    'mathScore':80,
+    "science Score":60
+};
+//대괄호 연산자
+console.log(student1["science Score"]);
+//점(닷, 마침표) 연산자
+console.log(student1.mathScore);
---
+++ 추가스터디js-2/js12-func2.js
@@ -0,0 +1,5 @@
+function printFruit(name, price){
+    console.log(name+"는 "+ price + "원 입니다.");
+}
+printFruit('banana',2000);
+
